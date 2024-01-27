367953e555576a99e32079953e92feb27f83cf50
Dylan Lu <dylanelu@gmail.com>
2024-01-27 15:03:47 -0800

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Dylan Lu on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
