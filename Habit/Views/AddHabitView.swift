2cf9e77c5e2c9a24d275f114393adf8ade9c987f
Dylan Lu <dylanelu@gmail.com>
2024-01-27 22:05:07 -0800

    stuff idk lol

--- Habit/Views/AddHabitView.swift
+++ Habit/Views/AddHabitView.swift
@@ -7,8 +7,22 @@
 
 import SwiftUI
 
+struct Day: Identifiable, Hashable {
+    let id: UUID
+    let name: String
+}
+
 struct AddHabitView: View {
     @State private var name: String = ""
+    @State private var selectedDays = Set<UUID>()
+    @State private var selectedTime = Date.now
+    
+    private var daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"]
+    private var days: [Day]
+    
+    init() {
+            days = daysOfWeek.map { Day(id: UUID(), name: $0) }
+    }
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -50,6 +64,33 @@ struct AddHabitView: View {
                             .padding(.horizontal, 4)
                         
                     }
+                    VStack(alignment: .leading) {
+                        Text("DAYS")
+                            .foregroundStyle(Theme.Color.gray400)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                        HStack {
+                            ForEach(days, id: \.self) { day in
+                                Button(action: {
+                                    if selectedDays.contains(day.id) {
+                                        selectedDays.remove(day.id)
+                                    } else {
+                                        selectedDays.insert(day.id)
+                                    }
+                                }) {
+                                    Text(day.name)
+                                        .padding()
+                                        .background(Theme.Color.gray700)
+                                        .foregroundColor(Theme.Color.gray200)
+                                        .cornerRadius(8)
+                                }
+                            }
+                        }
+                    }
+                    VStack() {
+                        DatePicker("Select what time", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                    }.padding()
+                        .foregroundColor(.white)
                 }
                 Spacer()
             }.padding()
